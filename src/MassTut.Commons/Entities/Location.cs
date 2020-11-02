abd18e3f514af573643a47ecc3df307783e87e51
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-11-02 17:49:23 -0300

    feat: Describe commons for MassTuT

---
+++ src/MassTut.Commons/Entities/Location.cs
@@ -0,0 +1,48 @@
+﻿namespace MassTut.Commons.Entities
+{
+    /// <summary>
+    /// A location.
+    /// </summary>
+    public class Location
+    {
+        protected Location()
+        {
+
+        }
+
+        public Location(long id, string title, double lat, double longi)
+        {
+            Id = id;
+            Title = title;
+            Latitude = lat;
+            Longitude = longi;
+        }
+
+        public bool IsNull => this is NullIsland;
+
+        public long Id { get; protected set; }
+        public string Title { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        /// <summary>
+        /// Nullable object for Locations.
+        /// </summary>
+        public static Location Null { get; } = new NullIsland();
+
+        /// <summary>
+        /// A null location
+        /// </summary>
+        class NullIsland : Location
+        {
+            public NullIsland()
+            {
+                Id = -1;
+                Title = "Null Island";
+                Latitude = -999d;
+                Longitude = -999d;
+            }
+        }
+    }
+
+}
---
+++ src/MassTut.Commons/Repositories/ILocationRepository.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using MassTut.Commons.Entities;
+
+namespace MassTut.Commons.Repositories
+{
+    /// <summary>
+    /// Describe methods to access Locations.
+    /// </summary>
+    public interface ILocationRepository
+    {
+        /// <summary>
+        /// Gets a single Location by its id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        Task<Location> GetById(long id, CancellationToken ct = default);
+
+        /// <summary>
+        /// Gets locations by a title.
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        IAsyncEnumerable<Location> GetByTitle(string title, CancellationToken ct = default);
+
+        /// <summary>
+        /// Creates a new Location.
+        /// </summary>
+        /// <param name="location"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        Task<long> Create(Location location, CancellationToken ct = default);
+
+        /// <summary>
+        /// Updates an existing location.
+        /// </summary>
+        /// <param name="location"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        Task Update(Location location, CancellationToken ct = default);
+
+        /// <summary>
+        /// Deletes an existing location.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        Task Delete(long id, CancellationToken ct = default);
+    }
+}
