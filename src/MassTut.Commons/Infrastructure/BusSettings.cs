1276c7516280b697a4ee5c7195359f173a5a64f1
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-11-10 19:21:37 -0300

    refactor: Move BusSettings into Commons

---
+++ src/MassTut.Commons/Infrastructure/BusSettings.cs
@@ -0,0 +1,38 @@
+﻿namespace MassTut.Keeper.Infrastructure
+{
+    /// <summary>
+    /// Settings for configuring the RabbitMQ bus.
+    /// </summary>
+    public class BusSettings
+    {
+        /// <summary>
+        /// Key where this configuration is held on.
+        /// </summary>
+        public const string SettingsKey = "RabbitMQ";
+
+        /// <summary>
+        /// Default credentials.
+        /// </summary>
+        protected const string DefaultCredentials = "guest";
+
+        /// <summary>
+        /// Address to connect to the RabbitMQ instance.
+        /// </summary>
+        public string Host { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Virtual Host to be used.
+        /// </summary>
+        public string VirtualHost { get; set; } = "/";
+
+        /// <summary>
+        /// Username to authenticate with.
+        /// </summary>
+        public string Username { get; set; } = DefaultCredentials;
+
+        /// <summary>
+        /// Password to authenticate with.
+        /// </summary>
+        public string Password { get; set; } = DefaultCredentials;
+    }
+}
